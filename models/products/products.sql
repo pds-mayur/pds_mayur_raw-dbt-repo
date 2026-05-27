b7f6f60bc084d33010e56d08a347b2c0c4882ec0
pds-mayur <mayur@profound-data.com>
2026-05-27 17:01:39 +0530

    Update products.sql

--- models/products/products.sql
+++ models/products/products.sql
@@ -1,4 +1,4 @@
--- dbt / Snowflake runtime error: division by zero
+-- dbt / Snowflake type conversion failure
 WITH source_data AS (
     SELECT *
     FROM RAW_DB.RAW_SCHEMA.PRODUCTS
@@ -10,9 +10,9 @@ cleaned_products AS (
         TRIM(product_name) AS product_name,
         UPPER(category) AS category,
         INITCAP(brand) AS brand,
-        CAST(unit_price AS NUMBER(10,2)) AS unit_price,
+        CAST('NOT_A_NUMBER' AS NUMBER(10,2)) AS unit_price,
         CAST(launch_date AS DATE) AS launch_date,
-        1 / 0 AS loaded_at
+        CURRENT_TIMESTAMP() AS loaded_at
     FROM source_data
 )
 
