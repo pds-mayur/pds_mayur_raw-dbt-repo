f16a2f9f664fe4a22695f9cf99a9356652fcf666
pds-mayur <mayur@profound-data.com>
2026-05-29 12:05:27 +0530

    Update products.sql

--- models/products/products.sql
+++ models/products/products.sql
@@ -1,4 +1,4 @@
--- dbt / Snowflake invalid argument type
+{{ config(materialized='table') }}
 
 WITH source_data AS (
     SELECT *
@@ -8,26 +8,3 @@ WITH source_data AS (
 SELECT
     SUM(product_name) AS total
 FROM source_data
-
-
--- corrected code
-
-/*WITH source_data AS (
-    SELECT *
-    FROM RAW_DB.RAW_SCHEMA.PRODUCTS
-)
-
-SELECT
-    SUM(price) AS total_price
-FROM source_data;*/
-
--- or
-
-/*WITH source_data AS (
-    SELECT *
-    FROM RAW_DB.RAW_SCHEMA.PRODUCTS
-)
-
-SELECT
-    COUNT(product_name) AS total_products
-FROM source_data;*/
