7bafdb898af40c827a76090a072fe6c1ca13fa9c
pds-mayur <mayur@profound-data.com>
2026-05-29 11:32:50 +0530

    Update products.sql

--- models/products/products.sql
+++ models/products/products.sql
@@ -1,4 +1,5 @@
--- dbt / Snowflake invalid identifier error
+-- dbt / Snowflake SQL compilation error
+
 WITH source_data AS (
     SELECT *
     FROM RAW_DB.RAW_SCHEMA.PRODUCTS
@@ -7,11 +8,11 @@ WITH source_data AS (
 cleaned_products AS (
     SELECT
         product_id,
-        TRIM(productName) AS product_name,
-        UPPER(category) AS category,
-        INITCAP(brand) AS brand,
-        CAST(unit_price AS NUMBER(10,2)) AS unit_price,
-        CAST(launch_date AS DATE) AS launch_date,
+        product_name,
+        category,
+        brand,
+        unit_price,   -- missing comma here
+        launch_date
         CURRENT_TIMESTAMP() AS loaded_at
     FROM source_data
 )
