016b50f2e0a286ec01b1da3d81dc4543ab4af1af
pds-mayur <mayur@profound-data.com>
2026-05-29 11:55:19 +0530

    Update products.sql

--- models/products/products.sql
+++ models/products/products.sql
@@ -1,19 +1,33 @@
--- dbt / Snowflake ambiguous column error
+-- dbt / Snowflake invalid argument type
 
-WITH orders_data AS (
-    SELECT *
-    FROM RAW_DB.RAW_SCHEMA.ORDER_ITEMS
-),
-
-products_data AS (
+WITH source_data AS (
     SELECT *
     FROM RAW_DB.RAW_SCHEMA.PRODUCTS
 )
 
 SELECT
-    product_id,
-    quantity,
-    product_name
-FROM orders_data od
-JOIN products_data pd
-    ON od.product_id = pd.product_id
+    SUM(product_name) AS total
+FROM source_data
+
+
+-- corrected code
+
+/*WITH source_data AS (
+    SELECT *
+    FROM RAW_DB.RAW_SCHEMA.PRODUCTS
+)
+
+SELECT
+    SUM(price) AS total_price
+FROM source_data;*/
+
+-- or
+
+/*WITH source_data AS (
+    SELECT *
+    FROM RAW_DB.RAW_SCHEMA.PRODUCTS
+)
+
+SELECT
+    COUNT(product_name) AS total_products
+FROM source_data;*/
