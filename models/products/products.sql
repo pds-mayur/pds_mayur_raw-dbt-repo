0d56b7afc7faf9d9a17e94d04b470d4fd4445f80
pds-mayur <mayur@profound-data.com>
2026-06-17 11:08:39 +0530

    Update products.sql

--- models/products/products.sql
+++ models/products/products.sql
@@ -1,17 +1,18 @@
--- dbt / Snowflake runtime error: division by zero
+-- dbt / Snowflake relation not found error
 WITH source_data AS (
     SELECT *
-    FROM RAW_DB.RAW_SCHEMA.PRODUCTS
+    FROM RAW_DB.RAW_SCHEMA.PRODUCTS_MISSING
 ),
 
 cleaned_products AS (
     SELECT
         product_id,
-        TRIM(productname) AS product_name,
+        TRIM(product_name) AS product_name,
         UPPER(category) AS category,
         INITCAP(brand) AS brand,
         CAST(unit_price AS NUMBER(10,2)) AS unit_price,
         CAST(launch_date AS DATE) AS launch_date,
+        CURRENT_TIMESTAMP() AS loaded_at
     FROM source_data
 )
 
