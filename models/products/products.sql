29b6ab9de9cf0f1026f5712424ce1163d503814f
pds-mayur <mayur@profound-data.com>
2026-06-25 22:39:13 +0530

    chore: incident remediation ffcc6ee1-8489-46d1-b2be-f99940b22550

--- models/products/products.sql
+++ models/products/products.sql
@@ -11,8 +11,7 @@ cleaned_products AS (
         UPPER(category) AS category,
         INITCAP(brand) AS brand,
         CAST(unit_price AS NUMBER(10,2)) AS unit_price,
-        CAST(launch_date AS DATE) AS launch_date,
-        1 / 0 AS loaded_at
+        CAST(launch_date AS DATE) AS launch_date
     FROM source_data
 )
 
