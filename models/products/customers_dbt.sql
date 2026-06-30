8e67f7f3559b0c265ebf9fe20e89210424f117ea
pds-mayur <mayur@profound-data.com>
2026-06-30 09:50:19 +0530

    Update customers_dbt.sql

--- models/products/customers_dbt.sql
+++ models/products/customers_dbt.sql
@@ -1,16 +1,6 @@
-WITH source_data AS (
-    SELECT *
-    FROM RAW_DB.RAW_SCHEMA.CUSTOMERS
-),
+{{ config(materialized='table') }}
 
-cleaned_customers AS (
-    SELECT
-        customer_id,
-        TRIM(customer_name) AS customer_name,
-        UPPER(customer_type) AS customer_type,
-        TO_NUMBER(customer_name) AS runtime_error
-    FROM source_data
-)
-
-SELECT *
-FROM cleaned_customers
+SELECT
+    *,
+    CAST('abc' AS NUMBER) AS runtime_error
+FROM RAW_DB.RAW_SCHEMA.CUSTOMERS
