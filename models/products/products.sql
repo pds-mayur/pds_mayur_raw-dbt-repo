86fd09c6651fae448af5323e67ed78b4475361e7
pds-mayur <mayur@profound-data.com>
2026-06-30 22:25:04 +0530

    Update products.sql

--- models/products/products.sql
+++ models/products/products.sql
@@ -1,21 +1,22 @@
--- dbt / Snowflake error: Window function not allowed in WHERE clause
--- Common when attempting to filter based on window function ranking directly in the WHERE clause instead of inside a CTE.
-WITH source_data AS (
-    SELECT *
-    FROM RAW_DB.RAW_SCHEMA.PRODUCTS
-),
+{{ config(
+    materialized='incremental',
+    unique_key='product_id'
+) }}
 
-ranked_products AS (
+WITH duplicated_customers AS (
+
     SELECT
         product_id,
-        product_name,
-        category,
-        unit_price,
-        ROW_NUMBER() OVER (PARTITION BY category ORDER BY unit_price DESC) AS price_rank
-    FROM source_data
-    -- ERROR: Placing window function directly in the WHERE clause is not allowed in SQL standard!
-    WHERE ROW_NUMBER() OVER (PARTITION BY category ORDER BY unit_price DESC) = 1
+        product_NAME
+    FROM RAW_DB.RAW_SCHEMA.products
+
+    UNION ALL
+
+    SELECT
+        product_id,
+        product_NAME
+    FROM RAW_DB.RAW_SCHEMA.products
 )
 
 SELECT *
-FROM ranked_products
+FROM duplicated_products
