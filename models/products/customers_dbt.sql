4a3d3030a0937fa9647830cbe5166f26c4e343f4
pds-mayur <mayur@profound-data.com>
2026-06-30 09:43:55 +0530

    Update customers_dbt.sql

--- models/products/customers_dbt.sql
+++ models/products/customers_dbt.sql
@@ -1,6 +1,16 @@
--- models/fct_customers_runtime_fail.sql
-select
-    customer_id,
-    customer_name,
-    to_number(customer_name) as boom
-from {{ ref('customers') }}
+WITH source_data AS (
+    SELECT *
+    FROM RAW_DB.RAW_SCHEMA.CUSTOMERS
+),
+
+cleaned_customers AS (
+    SELECT
+        customer_id,
+        TRIM(customer_name) AS customer_name,
+        UPPER(customer_type) AS customer_type,
+        TO_NUMBER(customer_name) AS runtime_error
+    FROM source_data
+)
+
+SELECT *
+FROM cleaned_customers
