21d6c0f26c9fe7907a2e53fd0b2f5f7f8e7da405
pds-mayur <mayur@profound-data.com>
2026-06-30 17:19:29 +0530

    Update products.sql

--- models/products/products.sql
+++ models/products/products.sql
@@ -1,4 +1,4 @@
--- dbt / Snowflake invalid identifier error
+-- dbt / Snowflake runtime error: division by zero
 WITH source_data AS (
     SELECT *
     FROM RAW_DB.RAW_SCHEMA.PRODUCTS
@@ -7,12 +7,12 @@ WITH source_data AS (
 cleaned_products AS (
     SELECT
         product_id,
-        TRIM(productName) AS product_name,
+        TRIM(product_name) AS product_name,
         UPPER(category) AS category,
         INITCAP(brand) AS brand,
         CAST(unit_price AS NUMBER(10,2)) AS unit_price,
         CAST(launch_date AS DATE) AS launch_date,
-        CURRENT_TIMESTAMP() AS loaded_at
+        1 / 0 AS loaded_at
     FROM source_data
 )
 
