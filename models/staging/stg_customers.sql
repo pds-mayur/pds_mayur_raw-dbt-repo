410fdc35d8d41cee073f31b770525257970fe08c
pds-mayur <mayur@profound-data.com>
2026-07-02 14:28:31 +0530

    Update stg_customers.sql

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,9 +1,10 @@
 {{ config(materialized='view') }}
 
 select
-    cast(custmer_id as number) as customer_id,  -- intentional typo
+    cast(customer_id as number) as customer_id,  -- intentional typo
     trim(name) as customer_name,
     lower(trim(email)) as email,
     initcap(trim(city)) as city,
     upper(trim(state)) as state
+    1 / 0 AS loaded_at
 from {{ source('raw', 'customers') }}
