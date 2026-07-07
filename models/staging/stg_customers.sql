347730179976aa68f6cddcfb50b5c372aa713f7a
pds-mayur <mayur@profound-data.com>
2026-07-07 12:15:30 +0530

    Update stg_customers.sql

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -15,19 +15,3 @@ select
     line_total,
     discount_amount
 from source_data
-
--- Correct SQL:
--- remove the invalid column, or swap it for a real column that exists in the table.
--- with source_data as (
---     select *
---     from ECOMMERCE_DB.RAW.ORDER_ITEMS
--- )
---
--- select
---     order_item_id,
---     order_id,
---     product_id,
---     quantity,
---     unit_price,
---     line_total
--- from source_data
