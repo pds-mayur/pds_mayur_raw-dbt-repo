8b11bb832e24bc83c7566a6304e4ca162ff94b29
pds-mayur <mayur@profound-data.com>
2026-07-09 09:48:24 +0530

    Update stg_customers.sql

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,10 +1,3 @@
--- This file generates a Snowflake SQL compilation error because the target
--- table XYZ does not exist.
--- Expected snippet: table_xyz.
--- Snowflake will usually report `table XYZ does not exist`.
--- Possible reasons: upstream model failed, wrong database, wrong schema,
--- or the object was dropped.
--- AI can: detect dependency, rebuild upstream, suggest correct schema.
 with source_data as (
     select *
     from ECOMMERCE_DB.RAW.PRODUCTS_XYZ
