620c8c3e886534d398d6d7ad2c633c5749f41047
pds-mayur <mayur@profound-data.com>
2026-07-13 13:25:08 +0530

    Update stg_customers.sql

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,17 +1,13 @@
-
-{{
-  config(
-    warehouse='NON_EXISTENT_WH',
+{{ config(
+    warehouse='COMPUTE_WH',
     materialized='table'
-  )
-}}
-
--- Force the warehouse switch
-{% do run_query("USE WAREHOUSE NON_EXISTENT_WH") %}
+) }}
 
 with source_data as (
     select *
     from ECOMMERCE_DB.RAW.CUSTOMERS
+    cross join ECOMMERCE_DB.RAW.CUSTOMERS c2
+    cross join ECOMMERCE_DB.RAW.CUSTOMERS c3
 )
 select *
 from source_data
