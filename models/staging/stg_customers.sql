da947ab4f0156e1a06c5a9f6cd18861d600ad0c6
pds-mayur <mayur@profound-data.com>
2026-07-13 11:36:41 +0530

    Update stg_customers.sql

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,8 +1,13 @@
 -- models/staging/stg_customers_missing.sql
-{{ config(
+{{
+  config(
     warehouse='NON_EXISTENT_WH',
     materialized='table'
-) }}
+  )
+}}
+
+-- Force the warehouse switch
+{% do run_query("USE WAREHOUSE NON_EXISTENT_WH") %}
 
 with source_data as (
     select *
