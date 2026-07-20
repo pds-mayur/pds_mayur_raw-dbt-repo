cc9baf58f47add94113291f91345c20178ed5868
pds-mayur <mayur@profound-data.com>
2026-07-20 10:31:32 +0530

    Refactor SQL to simplify data selection
    Removed unnecessary cleaned_orders CTE and adjusted selection from source_data.

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,18 +1,11 @@
--- dbt / Snowflake type conversion failure
 with source_data as (
     select *
     from ECOMMERCE_DB.RAW.ORDERS
-),
-
-cleaned_orders as (
-    select
-        order_id,
-        customer_id,
-        order_date,
-        status,
-        cast(status as number(10,2)) as loaded_at
-    from source_data
 )
 
-select *
-from cleaned_orders
+select
+    order_id,
+    customerid,
+    order_date,
+    status
+from source_data
