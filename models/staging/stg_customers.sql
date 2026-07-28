c526f1feb30679b5ac28294352ff71337a48ac0e
pds-mayur <mayur@profound-data.com>
2026-07-28 16:59:24 +0530

    Update stg_customers.sql

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,15 +1,11 @@
--- models/products.sql
--- Trying to cast a string column to date that contains invalid values
--- Will fail if description has non-date values
 with source_data as (
     select *
-    from ECOMMERCE_DB.RAW.PRODUCTS
+    from ECOMMERCE_DB.RAW.ORDERS
 )
 
 select
-    product_id,
-    product_name,
-    unit_price,
-    TRY_CAST(category AS DATE) AS invalid_date, 
-    created_at
+    order_id,
+    customerid,
+    order_date,
+    status
 from source_data
