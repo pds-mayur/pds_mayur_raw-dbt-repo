f0b8148763646cc607ef3d5a4ee4544eb96b1632
pds-mayur <mayur@profound-data.com>
2026-08-19 15:05:29 +0530

    Update fct_orders.sql

--- models/marts/orders/fct_orders.sql
+++ models/marts/orders/fct_orders.sql
@@ -1,11 +1,11 @@
 with source_data as (
     select *
-    from ECOERCE_DB.RAW.ORDERS
+    from ECOMMERCE_DB.RAW.ORDERS
 )
 
 select
     order_id,
-    customer_id,
+    customerid,
     order_date,
     status
 from source_data
