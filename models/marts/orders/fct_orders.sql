9b6c73c454c805f9e4ddd6c17b3207058dcda4b4
pds-mayur <mayur@profound-data.com>
2026-08-19 15:44:41 +0530

    Create fct_orders.sql

---
+++ models/marts/orders/fct_orders.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='table') }}
+
+select
+    column1 as order_id,
+    column2 as customer_id
+from values
+    (1, 101),
+    (2, 102),
+    (NULL, 103),
+    (NULL, 104),
+    (3, 105),
+    (NULL, 106);
