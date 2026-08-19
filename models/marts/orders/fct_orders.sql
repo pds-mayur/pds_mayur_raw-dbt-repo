373a097adc8b679e21638502020814cfb9eae1d9
pds-mayur <mayur@profound-data.com>
2026-08-19 15:47:42 +0530

    chore: incident remediation ab48c8ef-ecd5-4287-a2fb-4a7a89ddd198

--- models/marts/orders/fct_orders.sql
+++ models/marts/orders/fct_orders.sql
@@ -3,10 +3,10 @@
 select
     column1 as order_id,
     column2 as customer_id
-from values
+from (values
     (1, 101),
     (2, 102),
     (NULL, 103),
     (NULL, 104),
     (3, 105),
-    (NULL, 106);
+    (NULL, 106)) as orders;
