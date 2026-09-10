930c7c2a29acb2f1036e2e1024c65f4e6711e86a
pds-mayur <mayur@profound-data.com>
2026-09-10 16:22:43 +0530

    Update stg_stripe_orders.sql

--- models/staging/stripe/stg_stripe_orders.sql
+++ models/staging/stripe/stg_stripe_orders.sql
@@ -1,10 +1,10 @@
 with source_data as (
     select *
-    from AIE_TEST."public"."orders"
+    from ECOMMERCE_DB.RAW.ORDERS
 )
 
 select
-    "order_id",
-    "customer_id"
-    "order_date"
+    order_id,
+    customer_id,
+    order_date
 from source_data
