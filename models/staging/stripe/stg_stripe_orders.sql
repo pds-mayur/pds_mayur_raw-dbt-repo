a2f71f05c7f7b0c6ae5850bdfca1ca6cab13285a
pds-mayur <mayur@profound-data.com>
2026-09-10 13:17:19 +0530

    Update stg_stripe_orders.sql

--- models/staging/stripe/stg_stripe_orders.sql
+++ models/staging/stripe/stg_stripe_orders.sql
@@ -1,11 +1,11 @@
-{{ config(materialized='view') }}
-
 with source_data as (
-
     select *
-    from {{ source('stripe', 'orders') }}
-
+    from ECOMMERCE_DB.RAW.ORDERS
 )
 
-select *
+select
+    order_id,
+    customer_id,
+    order_date,
+    status
 from source_data
